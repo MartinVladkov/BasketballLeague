cbd50993659412a8b4b8e497effc86d84f834104
MartinVladkov <martin_vladkov@abv.bg>
2022-12-05 16:44:44 +0200

    Initial commit

---
+++ BasketballLeague API/BasketballLeague.API/BasketballLeague.API/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballLeague.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+       // public IActionResult Get()
+       // {
+       //
+       // }
+    }
+}
